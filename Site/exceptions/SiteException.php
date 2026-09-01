5b62c5cfc93564998f5e19ea5851a96052850a08
Colin Viebrock <colin@viebrock.ca>
2026-09-01 09:31:27 -0400

    Add tags to exceptions (searchable in sentry)

--- Site/exceptions/SiteException.php
+++ Site/exceptions/SiteException.php
@@ -1,19 +1,42 @@
 <?php
 
+declare(strict_types=1);
+
 /**
  * An exception in Site package.
  *
- * @copyright 2004-2016 silverorange
+ * @copyright 2004-2026 silverorange
  * @license   http://www.gnu.org/copyleft/lesser.html LGPL License 2.1
  */
 class SiteException extends SwatException
 {
-    public $title;
-    public $http_status_code = 500;
+    public ?string $title = null;
 
-    public function __construct($message = null, $code = 0)
+    public int $http_status_code = 500;
+
+    /**
+     * Additional context to add to the exception.
+     *
+     * This can be used (for example) to log info to Sentry without
+     * displaying it on the exception page, etc..
+     *
+     * @var array<string, mixed>
+     */
+    private array $context = [];
+
+    /**
+     * Tags to add to the exception.
+     *
+     * This can be used (for example) to log tags to Sentry which,
+     * unlike context, are searchable and filterable.
+     *
+     * @var array<string, string>
+     */
+    private array $tags = [];
+
+    public function __construct(string|Throwable|null $message = null, int $code = 0)
     {
-        if (is_object($message) && ($message instanceof PEAR_Error)) {
+        if ($message instanceof PEAR_Error) {
             $error = $message;
             $message = $error->getMessage();
             $message .= "\n" . $error->getUserInfo();
@@ -21,5 +44,29 @@ class SiteException extends SwatException
         }
 
         parent::__construct($message, $code);
+    }
+
+    final public function withContext(string $key, mixed $value): static
+    {
+        $this->context[$key] = $value;
+
+        return $this;
+    }
+
+    final public function getContext(): array
+    {
+        return $this->context;
+    }
+
+    final public function withTag(string $key, string $value): static
+    {
+        $this->tags[$key] = $value;
+
+        return $this;
+    }
+
+    final public function getTags(): array
+    {
+        return $this->tags;
     }
 }
